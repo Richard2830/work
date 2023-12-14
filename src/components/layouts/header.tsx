3d84ec02609c65b24b10df723d9691ac8b7d853b
Full Stack Super Guru <95362611+fantasyknight@users.noreply.github.com>
2023-12-14 07:43:07 +0200

    project

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -6,16 +6,16 @@ const Header: NextPage = () => {
         <div className="w-full flex justify-center fixed">
             <div className="w-full max-w-[1650px] ps-[24px] pe-[24px] flex flex-row items-center h-[70px] bg-[#faf3ea] font-[FoundersGrotesk] text-[15px] font-medium  rounded-bl-[10px] rounded-br-[10px]">
                 <div className="flex-1 flex justify-start">
-                    <HeaderButton children="left1" link="#" />
-                    <HeaderButton children="left2" link="#" />
-                    <HeaderButton children="left3" link="#" />
+                    <HeaderButton link="#">left1</HeaderButton>
+                    <HeaderButton link="#">left2</HeaderButton>
+                    <HeaderButton link="#">left3</HeaderButton>
                 </div>
                 <a href="#" className="w-[120px] text-[30px] flex items-center font-bold">
                     Yappy
                 </a>
                 <div className="flex-1 flex justify-end">
-                    <HeaderButton children="right1" link="#" />
-                    <HeaderButton children="right2" link="#" />
+                    <HeaderButton link="#">right1</HeaderButton>
+                    <HeaderButton link="#">right2</HeaderButton>
                 </div>
             </div>
         </div>
